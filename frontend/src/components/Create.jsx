77f93ed44aace1278915999c009c58c3c30c6e29
Nicholas Terrazas <nicholasterrazas01@gmail.com>
2024-03-11 23:32:49 -0400

    added (nonfunctional) buttons for adding to 1st/2nd script as well as an autocomplete search field with (nonfunctional) buttons

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import { OPCODES, nameToOpcode } from './Opcodes';
-import { Box, FormControlLabel, IconButton, ListItemButton, ListItemIcon, ListItemText, Stack, Switch } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Autocomplete, Box, Button, FormControlLabel, IconButton, ListItemButton, ListItemIcon, ListItemText, Stack, Switch, TextField, } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
+import LooksOne from '@mui/icons-material/LooksOne';
+import LooksTwo from '@mui/icons-material/LooksTwo';
 
 const label = { inputProps: { 'aria-label': 'Show disabled opcodes' } };
 
@@ -19,6 +21,31 @@ function OpcodeList({showDisabled, scriptOpcodes, setScriptOpcodes}) {
     return (
         <Box width='33%'>
             <h2>Opcodes</h2>
+
+            <Autocomplete 
+                disablePortal
+                id='opcode-autocomplete'
+                options={opcodes.map(op => ({...op, label: op.name}))}
+                sx={{width: 350}}
+                renderInput={(params) => <TextField {...params} label="Search Opcode" />}
+            />
+            <Stack direction='row'>
+                <Button
+                    sx={{width: 175}}
+                    fullWidth
+                    variant='contained' 
+                    onClick={() => console.log("adding to first")}    
+                >
+                    First
+                </Button>
+                <Button 
+                    sx={{width: 175}}
+                    variant='contained'
+                    onClick={() => console.log("adding to second")}
+                >
+                    Second
+                </Button>
+            </Stack>
             <List>
                 {opcodes.map(op => (
                     <ListItem 
@@ -26,9 +53,17 @@ function OpcodeList({showDisabled, scriptOpcodes, setScriptOpcodes}) {
                         style={{backgroundColor: op.disabled && '#ef5350'}}
                         disablePadding
                     >
-                        <ListItemButton onClick={() => addToScript(op)}>
-                            <ListItemText primary={`${op.name}`} secondary={`${op.hex}`}/>
-                        </ListItemButton>
+                        <ListItemText primary={`${op.name}`} secondary={`${op.hex}`}/>
+                        <ListItemIcon>
+                            <IconButton onClick={() => {console.log("adding to first"); addToScript(op);}}>
+                                <LooksOne />
+                            </IconButton>
+                        </ListItemIcon>
+                        <ListItemIcon>
+                            <IconButton onClick={() => {console.log("adding to second"); addToScript(op);}}>
+                                <LooksTwo />
+                            </IconButton>
+                        </ListItemIcon>                   
                     </ListItem> 
                 ))}
             </List>
@@ -54,7 +89,7 @@ function ScriptList({title, scriptOpcodes, setScriptOpcodes}) {
                             key={idx} 
                             secondaryAction={
                                 <IconButton edge='end' aria-label='trash' onClick={() => removeFromScript(idx)}>
-                                    <DeleteIcon />
+                                    <ClearIcon />
                                 </IconButton>
                             }
                             disablePadding 

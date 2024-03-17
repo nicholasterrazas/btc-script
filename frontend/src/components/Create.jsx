f80e465ab382d86bf92936c16e17b77a8fdca039
Nicholas Terrazas <nicholasterrazas01@gmail.com>
2024-03-16 23:12:02 -0400

    added collapsible lists for each opcode category

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -2,10 +2,8 @@ import * as React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import { OPCODES, P2MS, P2PK, P2PKH, RETURN } from './Opcodes';
-import { Autocomplete, Box, Button, ButtonGroup, FormControl, FormControlLabel, IconButton, InputLabel, ListItemButton, ListItemIcon, ListItemText, ListSubheader, MenuItem, Select, Stack, Switch, TextField, } from '@mui/material';
-import ClearIcon from '@mui/icons-material/Clear';
-import LooksOne from '@mui/icons-material/LooksOne';
-import LooksTwo from '@mui/icons-material/LooksTwo';
+import { Autocomplete, Box, Button, ButtonGroup, Collapse, Divider, FormControl, FormControlLabel, IconButton, InputLabel, ListItemButton, ListItemIcon, ListItemText, ListSubheader, MenuItem, Select, Stack, Switch, TextField, } from '@mui/material';
+import {Abc, Alarm, Calculate, Clear, ExpandLess, ExpandMore, FormatQuote, Layers, Lock, LooksOne, LooksTwo, Timer10, Timer10SelectSharp} from '@mui/icons-material/';
 
 
 function Settings({setShowDisabled, setShowPrefix, setShowHex}) {
@@ -45,12 +43,75 @@ function Settings({setShowDisabled, setShowPrefix, setShowHex}) {
 
 }
 
+function CategoryList({category, icon, opcodes, showHex, addToScript1, addToScript2}) {
+    const [open, setOpen] = React.useState(false);
+    
+    const handleClick = () => {
+        setOpen(!open);
+    };
+    
+    return (
+        <>
+            <ListItemButton onClick={handleClick} >
+                <ListItemIcon>
+                    {icon}
+                </ListItemIcon>
+                <ListItemText primary={`${category}`} />
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+        
+            <Collapse in={open} timeout="auto" unmountOnExit>
+            <Divider />
+            <List>
+                {opcodes.map(op => (
+                    <ListItem 
+                        key={`${op.label}`} 
+                        style={{color: op.disabled && '#ef5350'}}
+                    >
+                        <ListItemText primary={`${op.label}`} secondary={showHex && `${op.hex}`} />
+                        <ListItemIcon>
+                            <IconButton onClick={() => addToScript1(op)}>
+                                <LooksOne />
+                            </IconButton>
+                        </ListItemIcon>
+                        <ListItemIcon>
+                            <IconButton onClick={() => addToScript2(op)}>
+                                <LooksTwo />
+                            </IconButton>
+                        </ListItemIcon>                   
+                    </ListItem> 
+                ))}
+            </List>
+            <Divider />
+        </Collapse>
+      </>
+    );
+}
+
+
 function OpcodeList({showDisabled, showHex, scriptOpcodes1, setScriptOpcodes1, scriptOpcodes2, setScriptOpcodes2}) {    
     const [selectedOpcode, setSelectedOpcode] = React.useState(null);
 
     // use either all opcodes, or only opcodes that are not disabled 
-    let opcodes = showDisabled ? OPCODES : OPCODES.filter(op => !op.disabled); 
-    
+    let ops = showDisabled ? OPCODES : OPCODES.filter(op => !op.disabled);
+
+    let categoryAttributes = [
+        {name: 'Constants', icon: <Abc />}, 
+        {name: 'Stack', icon: <Layers />}, 
+        {name: 'Splice', icon: <FormatQuote />}, 
+        {name: 'Bitwise Logic', icon: <Timer10SelectSharp />}, 
+        {name: 'Arithmetic', icon:<Calculate /> }, 
+        {name: 'Crypto', icon: <Lock />}, 
+        {name: 'Locktime', icon: <Alarm />}
+    ];
+    let categories = categoryAttributes.map(category => (
+        {
+            title: category.name,
+            icon: category.icon,
+            opcodes: ops.filter(op => op.category === category.name.toLowerCase()),
+        }
+    ));
+
     const addToScript1 = (opcode) => {
         const newScriptOpcodes = [...scriptOpcodes1, opcode];
         setScriptOpcodes1(newScriptOpcodes);
@@ -68,7 +129,7 @@ function OpcodeList({showDisabled, showHex, scriptOpcodes1, setScriptOpcodes1, s
             <Autocomplete 
                 disablePortal
                 id='opcode-autocomplete'
-                options={opcodes}
+                options={ops}
                 sx={{width: 350}}
                 renderInput={(params) => <TextField {...params} label="Search Opcode" />}
                 onChange={(event, value) => setSelectedOpcode(value)}
@@ -92,26 +153,19 @@ function OpcodeList({showDisabled, showHex, scriptOpcodes1, setScriptOpcodes1, s
                     Second
                 </Button>
             </ButtonGroup>
-            <List>
-                {opcodes.map(op => (
-                    <ListItem 
-                        key={`${op.label}`} 
-                        style={{backgroundColor: op.disabled && '#ef5350'}}
-                        disablePadding
-                    >
-                        <ListItemText primary={`${op.label}`} secondary={showHex && `${op.hex}`} />
-                        <ListItemIcon>
-                            <IconButton onClick={() => addToScript1(op)}>
-                                <LooksOne />
-                            </IconButton>
-                        </ListItemIcon>
-                        <ListItemIcon>
-                            <IconButton onClick={() => addToScript2(op)}>
-                                <LooksTwo />
-                            </IconButton>
-                        </ListItemIcon>                   
-                    </ListItem> 
-                ))}
+            <List sx={{width: '100%'}}>
+                {categories.map(category => 
+                    <CategoryList 
+                        category={category.title}
+                        icon={category.icon}
+                        opcodes={category.opcodes}
+                        showHex={showHex}
+                        scriptOpcodes1={scriptOpcodes1}
+                        addToScript1={addToScript1}
+                        scriptOpcodes2={scriptOpcodes2}
+                        addToScript2={addToScript2}  
+                    />
+                )}
             </List>
         </Box>
     );
@@ -135,13 +189,13 @@ function ScriptList({showPrefix, showHex, title, scriptOpcodes, setScriptOpcodes
                             key={idx} 
                             secondaryAction={
                                 <IconButton edge='end' aria-label='trash' onClick={() => removeFromScript(idx)}>
-                                    <ClearIcon />
+                                    <Clear />
                                 </IconButton>
                             }
                             disablePadding 
                         >
                             <ListItemText 
-                                primary={showPrefix ? `${op.label}` : `${op.label.slice(3)}`} 
+                                primary={showPrefix || op.label.slice(0,3) !== 'OP_' ? `${op.label}` : `${op.label.slice(3)}`} 
                                 secondary={showHex && `${op.hex}`}/>
                         </ListItem>
                     ))}

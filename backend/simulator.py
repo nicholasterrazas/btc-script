b8c2c70a40fffd631efa50db47977bf99ab4eb53
Nicholas Terrazas <nicholasterrazas01@gmail.com>
2024-03-24 22:52:06 -0400

    started implementing unary opcodes

--- backend/simulator.py
+++ backend/simulator.py
@@ -25,10 +25,53 @@ def push_data(data: Data, stack: list[ScriptOp]) -> SimulationStep:
     return SimulationStep(script=script, stack=stack, message=message)
 
 
+def unary_operation(opcode: Opcode, script: list[ScriptOp], stack: list[ScriptOp]) -> SimulationStep:
+    operand = stack.pop(0)
+    
+    if opcode == OP_1ADD:
+        result = operand + 1
+        msg = f"Added 1 to <{operand}>\nPushing <{result}>"
+    elif opcode == OP_1SUB:
+        result = operand - 1
+        msg = f"Subtracted 1 from <{operand}>\nPushing <{result}>"
+    elif opcode == OP_NEGATE:
+        result = -operand
+        msg = f"Negated <{operand}>\nPushing <{result}>"
+    elif opcode == OP_ABS:
+        result = -operand if operand < 0 else operand
+        msg = f"Took absolute value of <{operand}>\nPushing <{result}>"
+    elif opcode == OP_NOT:
+        result = int(operand==0)
+        msg = f"Performed boolean 'NOT' on <{operand}>\nPushing <{result}>"
+    elif opcode == OP_0NOTEQUAL:
+        result = int(opcode!=0)
+        msg = f"Performed '0NOTEQUAL' on <{operand}>\nPushing <{result}>"
+    else:
+        msg = f"UNARY OPERATION ERROR: (OPCODE: {opcode}, OPERAND: {operand})"
+        return SimulationStep(script, stack, message=msg, failed=True)
+    
+    stack.insert(0, result)
+    return SimulationStep(script, stack, msg)
+
+
+def binary_operation(opcode: Opcode, script: list[ScriptOp], stack: list[ScriptOp]) -> SimulationStep:
+    ...
+
+
 def process_opcode(opcode: Opcode, stack: list[ScriptOp]) -> SimulationStep:
+    if opcode.disabled:
+        message = f"{opcode.label} is disabled"
+        return SimulationStep(script, stack, message, failed=True)
+    
     if not enough_args(opcode.arg_count, stack):
         message = f"{opcode.label} requires {opcode.arg_count} args but was given {len(stack)}"
         return SimulationStep(script, stack, message, failed=True)
+
+
+    if opcode.arg_count == 1:
+        return unary_operation(opcode, stack)
+    elif opcode.arg_count == 2:
+        return binary_operation(opcode, stack)
 
     # TODO: implement logic for each opcode
     stack.insert(0, opcode)

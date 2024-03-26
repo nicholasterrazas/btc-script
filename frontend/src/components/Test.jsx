5edcac69bbccea964d43e65dd84454ff71690f2c
Nicholas Terrazas <nicholasterrazas01@gmail.com>
2024-03-26 04:22:32 -0400

    started implementing 'select script' phase of simulation page

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -5,8 +5,82 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { IconButton, Stack, TextField, Tooltip } from '@mui/material';
+import { AutoFixHigh, Code } from '@mui/icons-material';
 
 const steps = ['Select Script', 'Simulate Script', 'Evaluate Script'];
+
+function Select() {
+    const [lock, setLock] = React.useState('');
+    const [key, setKey] = React.useState('');
+
+    const bothScriptsEmpty = (lock === '' && key === '');
+    const oneScriptEmpty = (lock === '' || key === '');
+
+    const scriptEditorNavigation = (
+        <Tooltip 
+            title={(bothScriptsEmpty ? 'Create' : 'Modify') + ' script using Script Editor'}
+            placement='right'
+        >
+            <IconButton 
+                aria-label='navigate to Create page to access Script Editor'
+                href='/create' disableTouchRipple
+            >
+                <AutoFixHigh htmlColor='gold'/>
+            </IconButton>
+        </Tooltip>
+    );
+
+    return (
+        <Box>
+
+            <Stack direction='column' justifyContent='center' alignItems='center' spacing={2}>
+                <Stack direction='row' justifyContent='center' alignItems='flex-start'>
+                    <Stack pl={5} direction='column' justifyContent='center' alignItems='center' spacing={2}>
+                
+                        <TextField
+                            id='Lock'
+                            label='Lock'
+                            sx={{ width: '500px' }}
+                            multiline
+                            onChange={(event) => setLock(event.target.value)}
+                        />
+
+                        <TextField
+                            id='Key'
+                            label='Key'
+                            sx={{ width: '500px' }}
+                            multiline
+                            onChange={(event) => setKey(event.target.value)}
+                        />
+                    </Stack>
+                    {scriptEditorNavigation}
+                </Stack>
+
+                <Button 
+                    variant='contained' 
+                    disabled={oneScriptEmpty} 
+                    endIcon={<Code htmlColor={!oneScriptEmpty && 'gold'}/>}
+                >
+                    Simulate Script
+                </Button>
+            </Stack>
+
+        </Box>
+    );
+}
+
+function Simulate() {
+    return (
+        null
+    );
+}
+
+function Evaluate() {
+    return (
+        null
+    );
+}
 
 function LinearStepper() {
     const [activeStep, setActiveStep] = React.useState(0);
@@ -119,8 +193,11 @@ export default function Test() {
 
     return (
         <>
-            <h1 id='test' style={{ paddingTop: '45px' }}>Test</h1>    
-            <LinearStepper />
+            <h1 id='test' style={{ paddingTop: '45px' }}>Test</h1>
+            <Stack alignItems='center' spacing={3}>
+                <LinearStepper />
+                <Select />
+            </Stack>
         </>
     );
 }
